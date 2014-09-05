63ceee66d3692052e8ff5bd34821947126017aac
zhongao <izhongao@gmail.com>
2014-09-06 05:23:04 +0800

    稳定API版本，初步测试通过

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ wechat-parser
 var express = require('express');
 var parser = require('wechat-parser');
 
-// 默认为 true，可设置为 false，设置后所有消息对象的键名为原始XML的映射
-parser.lowercase = false;
+// 默认为 false，可设置为 true，设置后所有消息对象的键名为小写
+parser.lowercase(false);
 
 // 默认为 false，设置后 req.weixin_xml 为原始XML字符串
-parser.originalXml = 'weixin_xml';
+parser.original('weixin_xml');
 
 var app = express();
 app.use(parser.middleware('weixin'));
@@ -51,10 +51,10 @@ __wechatParser.parse(stream, callback);__
 
 传入数据流参数（这里一般为`req`对象），通过回调返回解析后的微信消息对象。回调函数包含`err`和`message`2个参数。
 
-__wechatParser.lowercase__
+__wechatParser.lowercase(bool)__
 
-可配置的布尔属性，默认为`true`，设置是否将将返回的消息对象的键名小写。
+可配置的布尔属性，默认为`false`，设置是否将将返回的消息对象的键名小写。
 
-__wechatParser.originalXml__
+__wechatParser.original(name)__
 
 将原始的XML字符串挂载到`req`请求对象上，默认为`false`，可设置为字符串。
---
+++ test/parser.js
@@ -0,0 +1,110 @@
+
+var stream = require('stream');
+var should = require('should');
+var parser = require('..');
+var parse = parser.parse;
+var fs = require('fs');
+var path = require('path');
+
+describe('parse()', function() {
+
+
+  it('parse text message should ok', function(done) {
+    var text = path.join(__dirname, 'xmls/text.xml');
+    var stream = fs.createReadStream(text);
+    parse(stream, function(err, msg) {
+      (err == null).should.be.true;
+      msg.should.be.Object;
+      msg.should.have.property('MsgId');
+      msg.should.have.property('ToUserName');
+      msg.should.have.property('FromUserName');
+      msg.should.have.property('CreateTime');
+      msg.should.have.property('MsgType', 'text');
+      msg.should.have.property('Content');
+      done();
+    });
+  });
+
+
+  it('parse image message should ok', function(done) {
+    var image = path.join(__dirname, 'xmls/image.xml');
+    var stream = fs.createReadStream(image);
+    parse(stream, function(err, msg) {
+      (err == null).should.be.true;
+      msg.should.be.Object;
+      msg.should.have.property('MsgId');
+      msg.should.have.property('ToUserName');
+      msg.should.have.property('FromUserName');
+      msg.should.have.property('CreateTime');
+      msg.should.have.property('MsgType', 'image');
+      msg.should.have.property('PicUrl');
+      msg.should.have.property('MediaId');
+      done();
+    });
+  });
+
+
+  it('parse voice message should ok', function(done) {
+    var voice = path.join(__dirname, 'xmls/voice.xml');
+    var stream = fs.createReadStream(voice);
+    parse(stream, function(err, msg) {
+      (err == null).should.be.true;
+      msg.should.be.Object;
+      msg.should.have.property('MsgId');
+      msg.should.have.property('ToUserName');
+      msg.should.have.property('FromUserName');
+      msg.should.have.property('CreateTime');
+      msg.should.have.property('MsgType', 'voice');
+      msg.should.have.property('MediaId');
+      msg.should.have.property('Format');
+      done();
+    });
+  });
+
+
+  it('parse mass result event should ok', function(done) {
+    var result = path.join(__dirname, 'xmls/mass_send_job_finish.xml');
+    var stream = fs.createReadStream(result);
+    parse(stream, function(err, msg) {
+      msg.should.be.Object;
+      msg.should.have.property('ToUserName');
+      msg.should.have.property('FromUserName');
+      msg.should.have.property('CreateTime');
+      msg.should.have.property('MsgType', 'event');
+      msg.should.have.property('Event', 'MASSSENDJOBFINISH');
+      msg.should.have.property('Status');
+      msg.should.have.property('TotalCount');
+      msg.should.have.property('FilterCount');
+      msg.should.have.property('SentCount');
+      msg.should.have.property('ErrorCount');
+      msg.should.have.property('MsgID'); // 注意：这里的MsgID有所区别
+      done();
+    });
+  });
+
+
+  it('parse badmessage should not ok', function(done) {
+    var badxml = path.join(__dirname, 'xml.js');
+    var stream = fs.createReadStream(badxml);
+    parse(stream, function(err, msg) {
+      err.should.be.ok;
+      (msg == null).should.be.true;
+      done();
+    });
+  });
+
+
+  it('parse badxml should not ok', function(done) {
+    var badxml = path.join(__dirname, 'xmls/bad.xml');
+    var stream = fs.createReadStream(badxml);
+    parser.original('xml');
+    parse(stream, function(err, msg) {
+      (err == null).should.be.true;
+      msg.should.be.Object;
+      stream.xml.should.be.String;
+      done();
+    });
+  });
+
+
+});
